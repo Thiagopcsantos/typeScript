fe404f229d16424f4ea5031233b0a2a439f0e7aa
Thiago <thiagopcsantosmail.com>
2020-04-05 19:10:23 -0400

    first commit

---
+++ condicionales/switch.js
@@ -0,0 +1,18 @@
+var canticadeDeNumerosEnviados = 50;
+switch (canticadeDeNumerosEnviados) {
+    case 25: {
+        console.log("Son veite y cinco");
+        break;
+    }
+    case 20: {
+        console.log("Son veinte");
+        break;
+    }
+    case 15: {
+        console.log("Son Quince");
+        break;
+    }
+    default: {
+        console.log("No se cumplio nunguna función");
+    }
+}

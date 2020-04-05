25f9ec34c4fb7f71541ce9463d543fc97dbd200f
Thiago <thiagopcsantosmail.com>
2020-04-05 19:56:22 -0400

    switch.ts

--- condicionales/switch.ts
+++ condicionales/switch.ts
@@ -1,6 +1,4 @@
 var canticadeDeNumerosEnviados:number =50;
-
-
 switch (canticadeDeNumerosEnviados) {
 
     case 25:{
@@ -17,5 +15,27 @@ switch (canticadeDeNumerosEnviados) {
     }
     default:{
         console.log("No se cumplio nunguna función")
+    }
+}
+
+
+var menu:  number = 1
+
+switch(menu){
+    case 1:{
+        console.log("Accede a las canciones")
+        break;
+    }
+    case 2: {
+        console.log("Accede a los albuns")
+        break;
+    }
+    case 3 :{
+        console.log("Accede al perfil")
+        break;
+    }
+    default:{
+        console.log("El menu no existe")
+        break;
     }
 }

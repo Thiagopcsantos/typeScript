4c7f8351fd1ae951e5723b299d0e80197cc2a2ac
Thiago <thiagopcsantosmail.com>
2020-04-05 18:50:51 -0400

    first commit

---
+++ condicionales/elseif.js
@@ -0,0 +1,10 @@
+var cantidadSolicitada = 20;
+if (cantidadSolicitada > 15) {
+    console.log("Aplicariamos un gran descuento");
+}
+else if (cantidadSolicitada >= 10 && cantidadSolicitada <= 15) {
+    console.log("Aplicamos un descuento pequeño");
+}
+else {
+    console.log("No jace nada");
+}
---
+++ condicionales/elseif.ts
@@ -0,0 +1,9 @@
+var cantidadSolicitada: number = 20;
+
+if(cantidadSolicitada > 15 ){
+    console.log("Aplicariamos un gran descuento")
+}else if(cantidadSolicitada >= 10 && cantidadSolicitada <= 15){
+    console.log("Aplicamos un descuento pequeño")
+}else{
+    console.log("No jace nada")
+}

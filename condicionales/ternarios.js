cd21899c3fe27b115cf0ac0eaf5de095281aa720
Thiago <thiagopcsantosmail.com>
2020-04-05 20:41:29 -0400

    ciclos

---
+++ condicionales/ternarios.js
@@ -0,0 +1,8 @@
+var nombre;
+var edad;
+if (edad > 10) {
+    nombre = "Juan";
+}
+console.log(nombre);
+nombre = edad > 10 ? "Juan de ternario" : "No se cumplio";
+console.log(nombre);
---
+++ condicionales/ternarios.ts
@@ -0,0 +1,12 @@
+var nombre: string;
+var edad: number;
+
+
+if(edad > 10){
+    nombre ="Juan"
+}
+console.log(nombre);
+
+
+nombre = edad > 10 ? "Juan de ternario" : "No se cumplio";
+console.log(nombre);
